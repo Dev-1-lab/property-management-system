78c38a90b1f8be3f8785f6e09bcb2506d9ed0e65
javadev <bestactor301@gmail.com>
2025-11-15 15:21:22 +0500

    some changes

--- src/components/layout/SideBar.jsx
+++ src/components/layout/SideBar.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { Home, Package, Users, Shield, LogOut, FileText, BarChart, Send, Microscope, Scale, Gavel, DollarSign,LucideWarehouse } from 'lucide-react';
+import { Home, Package, Users, Shield, LogOut, FileText, BarChart, Send, Microscope, Scale, Gavel, DollarSign,LucideWarehouse ,ArrowRight,LucidePaperclip  } from 'lucide-react';
 import { useAuth } from '../../features/auth/context/AuthContext';
 import { USER_ROLES } from '../../utils/constants';
 
 const Sidebar = () => {
     const location = useLocation();
     const { user, logout } = useAuth();
-
     const menuItems = [
         {
             id: 'dashboard',
@@ -18,7 +17,7 @@ const Sidebar = () => {
         },
         {
             id: 'items',
-            label: 'Mol-mulkni olib qo\'yish',
+            label: 'Olib qo\'yilgan mol-mulklar',
             icon: Package,
             path: '/items',
             roles: [USER_ROLES.TERGOVCHI, USER_ROLES.TASDIQLOVCHI, USER_ROLES.MONITORING, USER_ROLES.ADMINISTRATOR],
@@ -40,7 +39,7 @@ const Sidebar = () => {
         {
             id: 'material',
             label: 'Materialni sudga topshirish',
-            icon: Send,
+            icon: LucidePaperclip  ,
             path: '/material',
             roles: [USER_ROLES.TERGOVCHI, USER_ROLES.ADMINISTRATOR],
         },
@@ -87,9 +86,9 @@ const Sidebar = () => {
     return (
         <div className="w-72 bg-indigo-800 text-white flex flex-col h-screen">
             {/* Header */}
-            <div className="p-6 border-b border-indigo-700">
+            <div className="p-3 border-b border-indigo-700">
                 <div className="flex items-center space-x-3">
-                    <Shield className="w-16 h-16" />
+                    <Shield className="w-20 h-20" />
                     <div>
                         <h1 className="text-lg font-bold">Mol-mulk xarakatini hisobga olish tizimi </h1>
                         <p className="text-xs text-indigo-200">{user?.role}</p>
@@ -98,7 +97,7 @@ const Sidebar = () => {
             </div>
 
             {/* Navigation */}
-            <nav className="flex-1 px-4 py-6 overflow-y-auto">
+            <nav className="flex-1 px-2 py-3 overflow-y-auto">
                 {filteredMenu.map((item) => (
                     <Link
                         key={item.id}
@@ -110,7 +109,7 @@ const Sidebar = () => {
                         }`}
                     >
                         <item.icon className="w-5 h-5" />
-                        <span className="font-normal text-sm">{item.label}</span>
+                        <span className="font-normal ">{item.label}</span>
                     </Link>
                 ))}
             </nav>
@@ -118,12 +117,12 @@ const Sidebar = () => {
             {/* User Info & Logout */}
             <div className="p-4 border-t border-indigo-700">
                 <div className="mb-3">
-                    <p className="text-sm font-medium">{user?.name}</p>
-                    <p className="text-xs text-indigo-200">{user?.email}</p>
+                    <p className="text-lg font-medium">{user?.name}</p>
+                    <p className="text-sm text-indigo-200">{user?.email}</p>
                 </div>
                 <button
                     onClick={handleLogout}
-                    className="flex items-center space-x-2 w-full px-4 py-2 rounded-lg hover:bg-indigo-700 transition-colors"
+                    className="flex items-center space-x-2 w-full  py-2 rounded-lg hover:bg-indigo-700 transition-colors cursor-pointer"
                 >
                     <LogOut className="w-5 h-5" />
                     <span>Chiqish</span>
